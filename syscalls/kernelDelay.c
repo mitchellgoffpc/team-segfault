9da42877c77b94221f04a48fc55537751b08cc37
emilyholt <emily@dali.dartmouth.edu>
2014-10-21 16:48:28 -0400

    delay updates
    v2

--- syscalls/kernelDelay.c
+++ syscalls/kernelDelay.c
@@ -2,17 +2,26 @@
 /*
 Method: delay the calling process for the given number of clock ticks
 */
-void kernelDelay(UserContext *) {
-	int clock_ticks = UserContext.regs[0];
-	//if clock_ticks = 0
-	//return immediately
+void KernelDelay(UserContext *) {
+	int clock_ticks;
+	clock_ticks = UserContext.regs[0];
 
-	//if clock_ticks < 0
-	//return ERROR
+	//if clock_ticks is 0 return immediately
+	if (clock_ticks == 0) return 0;
 
-	//int counter
-	//counter = 0
-	//while counter <= clock_ticks
+	//if clock_ticks < 0 don't time travel, return an Error
+	if (clock_ticks < 0) {
+		TracePrintf(1, "Hey! We can't go back in time!\n");
+		return -1;
+	}
+
 	//block the calling process until clock_ticks clock interrupts have occurred
+	int counter;
+	counter = 0;
+	while (counter < clock_ticks) {
+
+		counter++;
+	}
+	
 
 }
