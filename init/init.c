a9a0599251a249cc92e1f7db279bb7adea39415f
emilyholt <emily@dali.dartmouth.edu>
2014-10-21 20:05:02 -0400

    delay updates
    v3

--- init/init.c
+++ init/init.c
@@ -45,11 +45,43 @@ InterruptHandler interrupt_vector[TRAP_VECTOR_SIZE];
 
 // A dummy idle program for testing
 void DoIdle() {
-	TracePrintf(1, "DoIdle\n");
-	while(1) { }
+	
+	while(1) { 
+		TracePrintf(1, "DoIdle\n");
+		Pause();
+	}
 }
 
+/*	
+Method: delay the calling process for the given number of clock ticks
+*/
 
+void KernelDelay(UserContext *context) {
+	int clock_ticks;
+	clock_ticks = context->regs[0];
+
+	//if clock_ticks is 0 return immediately
+	if (clock_ticks == 0) return 0;
+
+	//if clock_ticks < 0 don't time travel, return an Error
+	if (clock_ticks < 0) {
+		TracePrintf(1, "Hey! We can't go back in time!\n");
+		return -1;
+	}
+
+	//block the calling process until clock_ticks clock interrupts have occurred
+	int counter;
+	counter = 0;
+	while (counter < clock_ticks) {
+		//put the idle process on the ready queue
+		//call a trapClock to kill the switch to
+		//the next process on the ready queue
+		//which will be doIdle 
+		
+	}
+	
+
+}
 
 
 /*
--- syscalls/delay.c
+++ syscalls/delay.c
@@ -1,10 +1,28 @@
-//Delay
 /*
+	File: Delay.c
+	Date 10/21/2014
+	Author: Emily Holt and Mitchell Goff
+*/
+
+
+/* =============================== *
+
+  	         Includes
+
+ * =============================== */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "init.c"
+/*	
 Method: delay the calling process for the given number of clock ticks
 */
-void KernelDelay(UserContext *) {
+
+
+void KernelDelay(UserContext *context) {
 	int clock_ticks;
-	clock_ticks = UserContext.regs[0];
+	clock_ticks = context->regs[0];
 
 	//if clock_ticks is 0 return immediately
 	if (clock_ticks == 0) return 0;
@@ -19,8 +37,8 @@ void KernelDelay(UserContext *) {
 	int counter;
 	counter = 0;
 	while (counter < clock_ticks) {
-
-		counter++;
+		//call the idle process
+		
 	}
 	
 
